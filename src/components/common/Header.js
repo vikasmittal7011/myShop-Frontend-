664efcda8ceaf7c21933b10a6c920a9c81595028
vikas gupta <vikasaggrawal700@gmail.com>
2023-08-13 17:46:16 +0530

    Add Gradient in header component

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-const Header = ({heading}) => {
+const Header = ({ heading }) => {
   return (
-    <header className="bg-white shadow">
+    <header
+      className="bg-white shadow"
+      style={{
+        background:
+          "linear-gradient(90deg, rgba(238, 130, 238, 1) 0%, rgba(0, 0, 255, 1) 100%)",
+      }}
+    >
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-bold tracking-tight text-gray-900">
           {heading}
