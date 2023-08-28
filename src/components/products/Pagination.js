a6eb1419bd4d13d5287844ad84eb9ffaf2a09816
vikas gupta <vikasaggrawal700@gmail.com>
2023-08-28 15:42:31 +0530

    Show pagination if condition is match

--- src/components/products/Pagination.js
+++ src/components/products/Pagination.js
@@ -15,78 +15,82 @@ const Pagination = ({ handlePage, page, totalProduct = 55 }) => {
   };
 
   return (
-    <div className="flex items-center justify-between border-t border-gray-200 px-4 py-3 sm:px-6 mt-6">
-      <div className="flex flex-1 justify-between sm:hidden">
-        <p
-          onClick={handlePrevious}
-          className={`${Classes.commonClass} ${Classes.commonBTNClass}`}
-        >
-          Previous
-        </p>
-        <p
-          onClick={handleNext}
-          className={`${Classes.commonClass} ${Classes.commonBTNClass} ml-3`}
-        >
-          Next
-        </p>
-      </div>
-      <div className="hidden sm:flex sm:flex-1 sm:items-start sm:justify-between sm:flex-col sm:space-y-3">
-        <div className="mr-2">
-          <p className="text-sm text-gray-700">
-            Showing{" "}
-            <span className="font-medium">
-              {(page - 1) * ITEM_PAGE_PER + 1}
-            </span>{" "}
-            to{" "}
-            <span className="font-medium">
-              {page * ITEM_PAGE_PER > totalProduct
-                ? totalProduct
-                : page * ITEM_PAGE_PER}
-            </span>{" "}
-            of <span className="font-medium">{totalProduct}</span> results
-          </p>
-        </div>
-        <div>
-          <nav
-            className="isolate inline-flex -space-x-px rounded-md shadow-sm"
-            aria-label="Pagination"
-          >
+    <>
+      {totalProduct > 10 && (
+        <div className="flex items-center justify-between border-t border-gray-200 px-4 py-3 sm:px-6 mt-6">
+          <div className="flex flex-1 justify-between sm:hidden">
             <p
               onClick={handlePrevious}
-              className={`${Classes.commonClass}  ${Classes.commonSreenBTNClass} rounded-l-md `}
+              className={`${Classes.commonClass} ${Classes.commonBTNClass}`}
             >
-              <span className="sr-only">Previous</span>
-              <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+              Previous
             </p>
-            {Array.from({
-              length: Math.ceil(totalProduct / ITEM_PAGE_PER),
-            }).map((item, index) => (
-              <p
-                key={index}
-                onClick={() => {
-                  handlePage(index + 1);
-                }}
-                aria-current="page"
-                className={`${
-                  page === index + 1
-                    ? `${Classes.activeClass} ${Classes.commonClass}`
-                    : `${Classes.hoverClass} ${Classes.commonClass}`
-                } cursor-pointer`}
-              >
-                {index + 1}
-              </p>
-            ))}
             <p
               onClick={handleNext}
-              className={`${Classes.commonClass}  ${Classes.commonSreenBTNClass} rounded-r-md`}
+              className={`${Classes.commonClass} ${Classes.commonBTNClass} ml-3`}
             >
-              <span className="sr-only">Next</span>
-              <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+              Next
             </p>
-          </nav>
+          </div>
+          <div className="hidden sm:flex sm:flex-1 sm:items-start sm:justify-between sm:flex-col sm:space-y-3">
+            <div className="mr-2">
+              <p className="text-sm text-gray-700">
+                Showing{" "}
+                <span className="font-medium">
+                  {(page - 1) * ITEM_PAGE_PER + 1}
+                </span>{" "}
+                to{" "}
+                <span className="font-medium">
+                  {page * ITEM_PAGE_PER > totalProduct
+                    ? totalProduct
+                    : page * ITEM_PAGE_PER}
+                </span>{" "}
+                of <span className="font-medium">{totalProduct}</span> results
+              </p>
+            </div>
+            <div>
+              <nav
+                className="isolate inline-flex -space-x-px rounded-md shadow-sm"
+                aria-label="Pagination"
+              >
+                <p
+                  onClick={handlePrevious}
+                  className={`${Classes.commonClass}  ${Classes.commonSreenBTNClass} rounded-l-md `}
+                >
+                  <span className="sr-only">Previous</span>
+                  <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+                </p>
+                {Array.from({
+                  length: Math.ceil(totalProduct / ITEM_PAGE_PER),
+                }).map((item, index) => (
+                  <p
+                    key={index}
+                    onClick={() => {
+                      handlePage(index + 1);
+                    }}
+                    aria-current="page"
+                    className={`${
+                      page === index + 1
+                        ? `${Classes.activeClass} ${Classes.commonClass}`
+                        : `${Classes.hoverClass} ${Classes.commonClass}`
+                    } cursor-pointer`}
+                  >
+                    {index + 1}
+                  </p>
+                ))}
+                <p
+                  onClick={handleNext}
+                  className={`${Classes.commonClass}  ${Classes.commonSreenBTNClass} rounded-r-md`}
+                >
+                  <span className="sr-only">Next</span>
+                  <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+                </p>
+              </nav>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
