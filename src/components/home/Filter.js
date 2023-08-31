fa5ce4440706475a761f1d502ca2c0deb880eee4
vikas gupta <vikasaggrawal700@gmail.com>
2023-08-31 20:39:10 +0530

    Fix show products to user

--- src/components/home/Filter.js
+++ src/components/home/Filter.js
@@ -1,37 +1,23 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { Menu } from "@headlessui/react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
-import {
-  fetchProductByFiltersAsync,
-  selectProducts,
-} from "../../features/product/productSlice";
 import { fetchAllBrandAsync } from "../../features/brand/brandSlice";
 import { fetchAllCategoryAsync } from "../../features/category/categorySlice";
-import { Classes, ITEM_PAGE_PER } from "../../utils/constant";
+import { Classes } from "../../utils/constant";
 import MobileFilter from "./MobileFilter";
 import OpenCloseBTN from "./OpenCloseBTN";
 import SortOptions from "./SortOptions";
 import FilterForm from "./FilterForm";
 import Buttons from "./Buttons";
 import Products from "../products/Products";
-import { selectuser } from "../../features/user/userSlice";
 
 export const Filter = () => {
-  const {
-    products: { products },
-    status,
-    totalItems,
-  } = useSelector(selectProducts);
-  const { userData } = useSelector(selectuser);
-
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
   const [filters, setFilters] = useState({ category: [], brand: [] });
 
   const [sort, setSort] = useState({});
-  const [page, setPage] = useState(1);
-
   const dispatch = useDispatch();
 
   const handleFilters = (id, e) => {
@@ -50,34 +36,11 @@ export const Filter = () => {
     setSort(sort);
   };
 
-  const handlePage = (page) => {
-    setPage(page);
-  };
-
-  useEffect(() => {
-    const pagination = { _page: page, _limit: ITEM_PAGE_PER };
-    dispatch(
-      fetchProductByFiltersAsync({
-        filters,
-        sort,
-        pagination,
-        admin: userData.role === "admin" && true,
-      })
-    );
-    dispatch(fetchAllBrandAsync());
-    dispatch(fetchAllCategoryAsync());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [sort, filters, page]);
-
   useEffect(() => {
     dispatch(fetchAllBrandAsync());
     dispatch(fetchAllCategoryAsync());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  useEffect(() => {
-    setPage(1);
-  }, [totalItems, sort]);
 
   return (
     <div>
@@ -110,16 +73,7 @@ export const Filter = () => {
             <FilterForm handleFilters={handleFilters} />
 
             <div className="lg:col-span-3">
-              {products && (
-                <Products
-                  handlePage={handlePage}
-                  page={page}
-                  setPage={setPage}
-                  products={products}
-                  status={status}
-                  totalItems={totalItems}
-                />
-              )}
+              <Products filters={filters} sort={sort} />
             </div>
           </div>
         </section>
--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,20 +1,50 @@
-import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import Loader from "../common/Loader";
 import Pagination from "./Pagination";
 import ProductList from "./ProductList";
 import AdminProductList from "./AdminProductList";
 import { selectuser } from "../../features/user/userSlice";
+import {
+  fetchProductByFiltersAsync,
+  selectProducts,
+} from "../../features/product/productSlice";
+import { ITEM_PAGE_PER } from "../../utils/constant";
 
-const Products = ({
-  handlePage,
-  page,
-  setPage,
-  products,
-  status,
-  totalItems,
-}) => {
+const Products = ({ filters, sort }) => {
+  const [page, setPage] = useState(1);
+  const dispatch = useDispatch();
+
   const { userData } = useSelector(selectuser);
+  const {
+    products: { products },
+    status,
+    totalItems,
+  } = useSelector(selectProducts);
+
+  const handlePage = (page) => {
+    setPage(page);
+  };
+
+  useEffect(() => {
+    if (userData) {
+      const pagination = { _page: page, _limit: ITEM_PAGE_PER };
+      dispatch(
+        fetchProductByFiltersAsync({
+          filters,
+          sort,
+          pagination,
+          admin: userData.role === "admin" ? true : false,
+        })
+      );
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sort, filters, page, userData]);
+
+  useEffect(() => {
+    setPage(1);
+  }, [totalItems, sort]);
 
   if (status !== "idle") {
     return <Loader />;
