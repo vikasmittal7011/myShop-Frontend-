95482f8b9e662edd54ec42ff5548e8017ee7d684
vikas gupta <vikasaggrawal700@gmail.com>
2023-09-04 19:39:14 +0530

    Add loader in login and signup page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,6 +6,7 @@ import logo from "../assets/logo.png";
 import Input from "../components/form/Input";
 import Links from "../components/common/Link";
 import Button from "../components/common/Button";
+import Loader from "../components/common/Loader";
 import {
   clearMessage,
   loginUserAsync,
@@ -92,62 +93,70 @@ const Login = () => {
   return (
     <>
       {token && <Navigate to="/" replace={true} />}
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img className="mx-auto h-16 w-auto" src={logo} alt="Your Company" />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
-          </h2>
-        </div>
+      {status !== "loading" ? (
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img
+              className="mx-auto h-16 w-auto"
+              src={logo}
+              alt="Your Company"
+            />
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-xl">
-          {message && (
-            <p className="text-red-600 my-3 font-bold text-2xl capitalize">
-              {message?.message}
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-xl">
+            {message && (
+              <p className="text-red-600 my-3 font-bold text-2xl capitalize">
+                {message?.message}
+              </p>
+            )}
+            <form className="space-y-6" onSubmit={handleSubmit}>
+              <Input
+                title="Email address"
+                id="email"
+                type="email"
+                placeHolder="Enter your gamil address..."
+                value={credentials.email}
+                onChange={manageCredentials}
+                errorMessage={errors.email}
+              />
+              <Input
+                title="Password"
+                id="password"
+                type="password"
+                showLink={true}
+                linkText="Forget Password?"
+                linkNavigate="/forgot-password"
+                placeHolder="Enter your password..."
+                value={credentials.password}
+                onChange={manageCredentials}
+                minLength={8}
+                errorMessage={errors.password}
+              />
+              <div>
+                <Button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Sign in
+                </Button>
+              </div>
+            </form>
+            <p className="mt-10 text-center text-sm text-gray-500">
+              Not a member?{" "}
+              <Links
+                to="/signup"
+                className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                name="Become a member"
+              />
             </p>
-          )}
-          <form className="space-y-6" onSubmit={handleSubmit}>
-            <Input
-              title="Email address"
-              id="email"
-              type="email"
-              placeHolder="Enter your gamil address..."
-              value={credentials.email}
-              onChange={manageCredentials}
-              errorMessage={errors.email}
-            />
-            <Input
-              title="Password"
-              id="password"
-              type="password"
-              showLink={true}
-              linkText="Forget Password?"
-              linkNavigate="/forgot-password"
-              placeHolder="Enter your password..."
-              value={credentials.password}
-              onChange={manageCredentials}
-              minLength={8}
-              errorMessage={errors.password}
-            />
-            <div>
-              <Button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Sign in
-              </Button>
-            </div>
-          </form>
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{" "}
-            <Links
-              to="/signup"
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-              name="Become a member"
-            />
-          </p>
+          </div>
         </div>
-      </div>
+      ) : (
+        <Loader />
+      )}
     </>
   );
 };
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -11,6 +11,7 @@ import {
   createUserAsync,
   selectauth,
 } from "../features/auth/authSlice";
+import Loader from "../components/common/Loader";
 
 const Signup = () => {
   const dispatch = useDispatch();
@@ -110,76 +111,84 @@ const Signup = () => {
   return (
     <>
       {token && <Navigate to="/" replace={true} />}
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img className="mx-auto h-12 w-auto" src={logo} alt="Your Company" />
-          <h2 className="mt-8 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Create a new account
-          </h2>
-        </div>
+      {status !== "loading" ? (
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img
+              className="mx-auto h-12 w-auto"
+              src={logo}
+              alt="Your Company"
+            />
+            <h2 className="mt-8 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Create a new account
+            </h2>
+          </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-xl">
-          <p className="text-red-600 my-3 font-bold text-2xl capitalize">
-            {message?.message}
-          </p>
-          <form className="space-y-6" onSubmit={handleSubmit}>
-            <Input
-              title="Name"
-              id="name"
-              type="text"
-              placeHolder="Enter your name..."
-              value={credentials.name}
-              onChange={manageCredentials}
-              errorMessage={errors.name}
-            />
-            <Input
-              title="Email address"
-              id="email"
-              type="email"
-              placeHolder="Enter your gamil address..."
-              value={credentials.email}
-              onChange={manageCredentials}
-              errorMessage={errors.email}
-            />
-            <Input
-              title="Password"
-              id="password"
-              type="password"
-              placeHolder="Enter your password..."
-              value={credentials.password}
-              onChange={manageCredentials}
-              minLength={8}
-              errorMessage={errors.password}
-            />
-            <Input
-              title="Confirm Password"
-              id="confirmPassword"
-              type="password"
-              placeHolder="Enter password again..."
-              value={credentials.confirmPassword}
-              onChange={manageCredentials}
-              minLength={8}
-            />
-            <p className="text-red-600">{errors.confirmPassword || null}</p>
-            <div>
-              <Button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Sign in
-              </Button>
-            </div>
-          </form>
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Alrady have a member?{" "}
-            <Links
-              to="/signin"
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-              name="Continue Shopping"
-            />
-          </p>
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-xl">
+            <p className="text-red-600 my-3 font-bold text-2xl capitalize">
+              {message?.message}
+            </p>
+            <form className="space-y-6" onSubmit={handleSubmit}>
+              <Input
+                title="Name"
+                id="name"
+                type="text"
+                placeHolder="Enter your name..."
+                value={credentials.name}
+                onChange={manageCredentials}
+                errorMessage={errors.name}
+              />
+              <Input
+                title="Email address"
+                id="email"
+                type="email"
+                placeHolder="Enter your gamil address..."
+                value={credentials.email}
+                onChange={manageCredentials}
+                errorMessage={errors.email}
+              />
+              <Input
+                title="Password"
+                id="password"
+                type="password"
+                placeHolder="Enter your password..."
+                value={credentials.password}
+                onChange={manageCredentials}
+                minLength={8}
+                errorMessage={errors.password}
+              />
+              <Input
+                title="Confirm Password"
+                id="confirmPassword"
+                type="password"
+                placeHolder="Enter password again..."
+                value={credentials.confirmPassword}
+                onChange={manageCredentials}
+                minLength={8}
+              />
+              <p className="text-red-600">{errors.confirmPassword || null}</p>
+              <div>
+                <Button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Sign in
+                </Button>
+              </div>
+            </form>
+            <p className="mt-10 text-center text-sm text-gray-500">
+              Alrady have a member?{" "}
+              <Links
+                to="/signin"
+                className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                name="Continue Shopping"
+              />
+            </p>
+          </div>
         </div>
-      </div>
+      ) : (
+        <Loader />
+      )}
     </>
   );
 };
