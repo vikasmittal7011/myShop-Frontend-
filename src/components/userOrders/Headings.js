6fa852de83d34699d3580ca3f8938b7f571e355f
vikas gupta <vikasaggrawal700@gmail.com>
2023-09-04 19:34:26 +0530

    Show order placed date to user at order page

--- src/components/userOrders/Headings.js
+++ src/components/userOrders/Headings.js
@@ -1,4 +1,4 @@
-import React from "react";
+import dayjs from "dayjs";
 
 const Headings = ({ order }) => {
   return (
@@ -6,6 +6,9 @@ const Headings = ({ order }) => {
       <h1 className="font-bold lg:text-2xl md:text-2xl sm:text-2xl l:text-2xl s:text-2xl xs:text-[1.3rem] mb-3">
         Order Id: #{order.id}
       </h1>
+      <h2 className="font-bold text-xl mb-3 text-green-500">
+        Order On: {dayjs(order.createdAt).format("YYYY-MM-DD HH:mm:ss")}
+      </h2>
       <h2 className="font-bold text-xl mb-3 text-green-500">
         Order Status: {order.status}
       </h2>
