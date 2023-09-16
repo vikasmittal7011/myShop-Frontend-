bf47b193b1d7979204510cee5c5738db5d4506df
vikas gupta <vikasaggrawal700@gmail.com>
2023-09-16 17:26:40 +0530

    Make carousel of product images when screen small

--- src/components/products/Images.js
+++ src/components/products/Images.js
@@ -1,8 +1,12 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 import Image from "../common/Image";
 
 const Images = ({ images }) => {
   return (
     <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
+      {/* Large screens */}
       <div className="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
         {images[0] && (
           <Image src={process.env.REACT_APP_API + images[0]} alt={images[0]} />
@@ -26,10 +30,30 @@ const Images = ({ images }) => {
           )}
         </div>
       </div>
-      <div className="aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden rounded-lg s:mx-3 l:mx-3 xs:mx-3">
-        {images[3] && (
-          <Image src={process.env.REACT_APP_API + images[3]} alt={images[3]} />
-        )}
+      {/* Hide this image on all screen sizes */}
+      <div className="sm:hidden l:hidden s:hidden xs:hidden md:hidden lg:block xl:block 2xl:block">
+        <div className="aspect-h-5 aspect-w-4 overflow-hidden rounded-lg h-full">
+          {images[3] && (
+            <Image
+              src={process.env.REACT_APP_API + images[3]}
+              alt={images[3]}
+            />
+          )}
+        </div>
+      </div>
+      {/* Auto-scrolling carousel for small screens */}
+      <div className="lg:hidden mx-5">
+        <Carousel autoPlay interval={2000} infiniteLoop showThumbs={false}>
+          {images.map((image, index) => (
+            <div key={index} className="aspect-h-5 aspect-w-4">
+              <Image
+                src={process.env.REACT_APP_API + image}
+                alt={image}
+                className="object-cover w-full h-full rounded-md"
+              />
+            </div>
+          ))}
+        </Carousel>
       </div>
     </div>
   );
